187b7c37ddef04de57551ac09d74b2b63e23f0e1
Alessandro Metta <n0f3@outlook.com>
2016-08-18 21:17:04 +0000

    Deleting files after sending request back

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let path = require('path');
 let app = express();
 let multer = require('multer');
 let uploadFile = multer({dest: './uploads/'});
+let fs = require('fs');
 const htmlDir = path.join(__dirname, 'public', 'html');
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,6 +16,17 @@ app.get('/', (req, res) => {
 });
 
 app.post('/processupload', uploadFile.single('fileUpload'), (req, res) => {
+  const fileSize = req.file.size;
+  const fileName = req.file.filename;
+  fs.stat('./uploads', (err, stats) => {
+    if(err) throw err;
+    if(stats.isDirectory()) {
+      fs.unlink(req.file.path, (err) => {
+        if (err) throw err;
+        console.log('Deletion sucessful.');
+      });
+    }
+  })
   return res.send({ fileSize: req.file.size });
 });
 
